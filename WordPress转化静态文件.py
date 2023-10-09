96ae9e4f415eb7fe978541afa8f11f0da5bc2c77
jiubanyipeng <53213779+jiubanyipeng@users.noreply.github.com>
2023-10-09 14:44:45 +0800

    在命令行下添加任意参数便重新缓存

--- WordPress转化静态文件.py
+++ WordPress转化静态文件.py
@@ -2,10 +2,10 @@
 # WordPress转成静态html,在命令行下任意添加一个参数便重新缓存
 import requests
 from xmltodict import parse
-import sys
+import sys,time,os
 
 
-# 获取url列表
+# 获取url列表，返回的是地址列表 ['https://www.abc.com/2.html','https://www.abc.com/3.html']
 def get_url(url_list):
     try:
         url_lists = []  # 遍历所有地址存到这里
@@ -13,6 +13,7 @@ def get_url(url_list):
             date_url = parse(requests.post(i).text)
             for url in date_url.get('urlset', {}).get('url', {}):
                 url_lists.append(url.get('loc',''))
+
         return url_lists
     except Exception as e:
         print('错误：',e)
@@ -20,31 +21,48 @@ def get_url(url_list):
 
 
 def get_text(url,url_lists):
-    with open('url.txt', 'r', encoding='utf-8') as f:
-        in_url = f.read().split('\n')
-    for i in url_lists:
-        name = i.replace(url,'')
-        if '/' in name:
-            print('存在子分类，由于文件夹和文件不能同名，这里就不在做子分类的延续')
-            continue
-        # 已经缓存的文件就不再缓存
+    if not os.path.exists('./url.txt'):
+        with open('./url.txt', 'w') as f:
+            print('url.txt 文件不存在 已创建文件')
+    with open('./url.txt', 'r') as f1:
+        in_url = f1.read().strip().split('\n')
+        # 缓存操作
         if len(sys.argv) > 1:
-            print(f'{name}重新缓存中...')
-        else:
+            print('重新缓存中...')
+            for s in in_url:
+                if s == '':
+                    s = 'index.html'
+                print(f'删除： {s}')
+                try:
+                    os.remove(s)
+                except Exception as e:
+                    print(f'{s} 删除失败！')
+            in_url = []
+            with open('./url.txt', 'w', encoding='utf-8') as ff:
+                pass
+            print('清空缓存完成，正在重新获取')
+        for i in url_lists:
+            name = i.replace(url,'')
+            if '/' in name:
+                print('存在子分类，由于文件夹和文件不能同名，这里就不在做子分类的延续')
+                continue
+            # 已经缓存的文件就不再缓存
             if name in in_url:
                 continue
-        try:
-            html = requests.post(i).text
-            if name == '':
-                name = 'index.html'
-            with open(f'{name}','w',encoding='utf-8') as ff:
-                ff.write(html)
-            with open('url.txt', 'a', encoding='utf-8') as fff:
-                fff.write(name + '\n')
-        except Exception as e:
-            print(e)
+            try:
+                print(f'获取: {i}')
+                time.sleep(1)  # 防止太快被服务器防火墙封禁
+                html = requests.post(i).text
+                if name == '':
+                    name = 'index.html'
+                with open(f'{name}','w',encoding='utf-8') as ff:
+                    ff.write(html)
+                with open('./url.txt', 'a', encoding='utf-8') as fff:
+                    fff.write(name + '\n')
+            except Exception as e:
+                print(e)
 
 
 url = 'https://www.jiubanyipeng.com/'   # 这里是用来替换使用的，一定要完整的url地址且要加上 /
-url_list = [url+'post-sitemap.xml',  url+'page-sitemap.xml', url+'category-sitemap.xml']  # 文章 插件内链 插件内链 分类  标签  等自己添加进来
+url_list = [url+'post-sitemap.xml',  url+'page-sitemap.xml', url+'category-sitemap.xml']  # 网站地图，不一定兼容（本站文章 插件内链 分类  标签）
 get_text(url,get_url(url_list))
